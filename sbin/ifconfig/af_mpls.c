708474f9bd26b8a75b52d2123b184ed4d5e7793e
tsag <gast@morpheus.testenv.local>
2015-07-23 04:29:24 +0200

    Added modefied route(8) and ifconfig(8) commands.
    Beware, modifications are under active developement. There
    is some likelyhood that present implementation could change
    due to ongoining testing and review.
    
    Modifications targeting ifconfig(8) due to MPLS label
    assignement on interface scope in context of if_mpe(4)
    are not correct implemented... be patient.
    
    Further, I've started singning commits.
    
    If someone wants to contact me by electronic mail, please
    use encryption.
    
    Our world is not a (so called) "Ponyhof", yeah. :-).
    
    -----BEGIN PGP PUBLIC KEY BLOCK-----
    Version: GnuPG v1
    
    mQGiBFWePMgRBAC6W4aGo1ppG3ZFL1lVKNHxSwsP0NEKl3nl2lPyveLkwFEFeEYu
    H7zfsqkmcHK1CpuFha3GaqS2OIciFxsVEB62ikjum4hPCWa/iw+Y0EOYmcoDDLmX
    2FRIoPrbbJ0V5jEktrRKNnS5nvywIDLh+ORGnq0aws8usSs/ySSQ6PuQJwCg2j0t
    7AlAnn3OuCYcv0GsimRdFZMD/1OPAVtY1mVB+Vrd0H1wLzLDdFS6Vx4O6rViwP0N
    5s1KRhqoMll1S3EQC/lhAu+s9b50owDSUGCiXHfpbyB7iUISFlAOV9QQ4afd6ae0
    RlpIu482PnrdfVSJEEQDBDj/BpSVAFK6H03Pl78E+b0l9cWuzeYqrEf9sfF8OH8j
    mK9LA/9RYdyHGVUaBFIOuoQkAyKT7CyJggVmuO6C5Y3tTDTAc/w+pw/2uI0kYOpQ
    CC4ndyhRG3FU4aNOTaPc1PF5uYIrEHUyEnfFkyULAMYfn9p36Blnqo5zh/YjVL69
    3CVg4ctv7ZmQePyToD+C48xY4YTGu6Gf8XjUfCATz+teMd7u2rRHSGVubmluZyBN
    YXR5c2Nob2sgKEtvbW1lbnRhci4pIDxoZW5uaW5nLm1hdHlzY2hva0BzdHVkLmZo
    LWZsZW5zYnVyZy5kZT6IYAQTEQIAIAUCVZ48yAIbAwYLCQgHAwIEFQIIAwQWAgMB
    Ah4BAheAAAoJEG5NpFEQWRHK724AniVS+UsNSrAg0/mQkf9Fll7x07zNAJ9CMB97
    aDCoGpagfg3sXvs2au/3jrkCDQRVnjzIEAgApaGHl4vvyPGy0nOI1hbQ4p3nM5+S
    sWl/uxp2alXacRlKYkHkD8RsR5lJRjG17tuLpiogHYhTt8ijRplwCp2b2FTRq8aF
    ULTewRkjOVamVjtnaa5lQ0/Z3QH+f5BK5dcboZHJlQg/+LOgFNpVF/nUaTF+jfVs
    DnZVLUhJeY+LYpcw60NEu9yiUWxMD/944xoXC+uVb1ihsDxVSFgpFFjhpkN0Bcg9
    aii6o3Eyhqr4dKZsc52p/NmZWPtMxOaPYqYPBZOgs69JedL6zAdAwel9sNJ0aaJy
    F4bRvQfy5tryhy5j83MSQ/594l1nGCvUHvYez5sddHkyDjWy4FqYm5SCwwADBgf9
    EpPcdF7F+OckvlJ1BbjtufOayORknIKSMrbfD2tj88brp9awl77Yw4iKegfSyw/p
    vCvyrjyZiy729lVlvv8jah3oISx+jcfpSgB1QnXHhWz1vDkgc66nn7trXgAlHoH0
    TdaAzOMtu59vgcQe8FbBwLOI90DjNnuwbwRw0nXvRxiJCQd5aiYb2LRjlvo+6L3T
    J8fJirqBjmFzGpcH1haZo1GYGmr5MsUJMEpwxr77YKBji32ca/V1uyBko/DbZrW7
    oUAYr+GawaCwb13Y8CsyFl3FAWRVBYJ0Y1iO/3W2QpGLcpitcnKxrnCFFYSbLsoc
    +6qGvUjOSFvHUtDajNoIfYhJBBgRAgAJBQJVnjzIAhsMAAoJEG5NpFEQWRHKZrEA
    n0Bj9G0snst74mnx/jvBuoQQZKtjAKCd8pZpq2rFKgCrJp+BjXpDfetMMw==
    =SMc1
    -----END PGP PUBLIC KEY BLOCK-----

---
+++ sbin/ifconfig/af_mpls.c
@@ -0,0 +1,262 @@
+/*-
+ * Copyright (c) 2014, 2015 Henning Matyschok
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/if.h>
+
+#include <ctype.h>
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ifaddrs.h>
+
+#include <net/if_dl.h>
+#include <net/if_types.h>
+#include <net/if_var.h>	
+
+#include <netinet/in.h>
+#include <netinet/in_var.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <netmpls/mpls.h>
+
+#define MPLS_ADDRBUFSIZ 	(MAXHOSTNAMELEN * 2 + 1)
+#define MPLS_WORKBUFSIZ 	(MPLS_ADDRBUFSIZ * 2 + 1)
+
+static struct mpls_aliasreq mpls_addreq;
+
+#include "ifconfig.h"
+
+static void
+mpls_status(int s __unused, const struct ifaddrs *ifa)
+{
+	char abuf[MPLS_ADDRBUFSIZ], wbuf[MPLS_WORKBUFSIZ], *cq;
+	uint32_t iseg, op, oseg;
+
+	if ((ifa == NULL) 
+		|| ((ifa->ifa_addr) == NULL) 
+		|| ((ifa->ifa_dstaddr) == NULL))
+		return; 
+
+	iseg = ntohl(satosmpls_label(ifa->ifa_addr)) >> MPLS_LABEL_OFFSET;
+	op = satosftn_op(ifa->ifa_dstaddr);
+	oseg = ntohl(satosftn_label(ifa->ifa_dstaddr)) >> MPLS_LABEL_OFFSET;
+	
+	cq = wbuf;
+	cq += sprintf(cq, "\tmpls %d ", iseg);	
+
+	switch (op) {
+	case RTF_POP:
+		cq += sprintf(cq, "pop ");
+		break;
+	case RTF_PUSH:
+		cq += sprintf(cq, "psh ");
+		break;
+	case RTF_SWAP:
+		cq += sprintf(cq, "swp ");
+		break;
+	default:
+		break;
+	}
+	
+	if (iseg != oseg)
+		cq += sprintf(cq, "%d ", oseg);
+	
+	switch (ifa->ifa_dstaddr->sa_family) {
+	case AF_INET:
+		(void)snprintf(abuf, MPLS_ADDRBUFSIZ, "%s", 
+			inet_ntoa(((struct sockaddr_in *)
+			ifa->ifa_dstaddr)->sin_addr));
+		cq += sprintf(cq, "-> %s", abuf);
+		break;
+	case AF_INET6:
+		(void)inet_ntop(AF_INET6, 
+			&((struct sockaddr_in6 *)
+			ifa->ifa_dstaddr)->sin6_addr, 
+			abuf, MPLS_ADDRBUFSIZ);	
+		cq += sprintf(cq, "-> %s", abuf);
+		break;
+	case AF_MPLS: 
+		cq += sprintf(cq, "-> %d (downstream)", 
+			ntohl(satosmpls_label(ifa->ifa_dstaddr)) 
+				>> MPLS_LABEL_OFFSET);
+		break;
+	case AF_LINK: {
+/*
+ * Following code-section is reused from implementation 
+ * of in af_link.c defined link_status procedure, part of
+ * ifconfig(8) implementation. 
+ */	
+ 		struct sockaddr_dl *sdl = (struct sockaddr_dl *)ifa->ifa_dstaddr;
+ 
+		if (sdl != NULL && sdl->sdl_alen > 0) {
+			if ((sdl->sdl_type == IFT_ETHER 
+				|| sdl->sdl_type == IFT_L2VLAN 
+				|| sdl->sdl_type == IFT_BRIDGE) 
+				&& sdl->sdl_alen == ETHER_ADDR_LEN)
+				cq += sprintf(cq, "-> ether %s\n",
+				    ether_ntoa((struct ether_addr *)LLADDR(sdl)));
+			else {
+				int n = sdl->sdl_nlen > 0 ? sdl->sdl_nlen + 1 : 0;
+	
+				cq += sprintf(cq, "-> link %s\n", link_ntoa(sdl) + n);
+			}
+		}		
+		break;
+	}
+	default: 
+		cq += sprintf(cq, "n/a");
+		break;
+	}
+	(void)printf("%s", wbuf);
+	putchar('\n');
+}
+
+/*
+ * Request by mpls_control per interface MPLS label binding.
+ */
+static void
+mpls_getaddr(const char *s, int which)
+{
+	struct mpls_aliasreq *ifra = &mpls_addreq;
+	struct sockaddr *seg;
+	uint32_t label;
+	
+	if (which != ADDR) 
+		return;
+	
+	seg = (struct sockaddr *)&ifra->ifra_seg;
+	
+	label = strtoul(s, (char **)NULL, 10);	
+	if (label > MPLS_LABEL_MAX)
+		errx(1, "%s: bad value", s);
+	
+	label = htonl(label << MPLS_LABEL_OFFSET);
+		
+	seg->sa_len = SFTN_LEN;
+	seg->sa_family = AF_MPLS; 
+	
+	satosmpls_label(seg) = label;
+	satosftn_label(seg) = label;
+}
+
+/*
+ * Bind link layer interface on if_mpe(4) 
+ * performing proxyfied transmission. 
+ */
+static void
+mpls_setproxy(const char *val, int d, int s, 
+		const struct afswtch *afp)
+{
+	struct ifaliasreq ifra;
+	struct sockaddr_mpls *smpls;
+	struct sockaddr_dl *sdl;
+	size_t nlen;
+
+	if ((nlen = strnlen(val, IFNAMSIZ)) >= IFNAMSIZ)
+		errx(1, "%s: bad value", val);
+
+	(void)memset(&ifra, 0, sizeof(ifra));
+	(void)memcpy(ifra.ifra_name, name, IFNAMSIZ);	
+	
+	smpls = (struct sockaddr_mpls *)&ifra.ifra_addr;
+	smpls->smpls_len = sizeof(*smpls);
+	smpls->smpls_family = AF_MPLS;
+	
+	sdl = (struct sockaddr_dl *)&ifra.ifra_broadaddr;
+	sdl->sdl_len = sizeof(*sdl);
+	sdl->sdl_family = AF_LINK;	
+	sdl->sdl_nlen = nlen;
+	
+	(void)memcpy(sdl->sdl_data, val, nlen);
+
+	if (ioctl(s, SIOCSIFPHYADDR, &ifra) < 0)  
+		errx(1, "invalid link");
+} 	
+
+/*
+ * Removes link layer interface bonding.
+ */ 
+static void
+mpls_rmproxy(const char *val, int d, int s, 
+		const struct afswtch *afp)
+{
+	struct ifreq ifr;
+	struct sockaddr_mpls *smpls;
+	
+	(void)memset(&ifr, 0, sizeof(ifr));
+	(void)memcpy(ifr.ifr_name, name, IFNAMSIZ);
+
+	smpls = (struct sockaddr_mpls *)&ifr.ifr_addr;
+	smpls->smpls_len = sizeof(*smpls);
+	smpls->smpls_family = AF_MPLS;
+	
+	if (ioctl(s, SIOCDIFPHYADDR, &ifr) < 0) 
+		errx(1, "invalid link");
+}
+
+/* 
+ * Command table targeting if_mpe(4). 
+ */
+static struct cmd mpe_cmds[] = {
+	DEF_CMD_ARG("proxy", mpls_setproxy),
+	DEF_CMD("-proxy", 0, mpls_rmproxy),
+};
+
+/* 
+ * Adress family specific descriptor. 
+ */
+static struct afswtch af_mpls = {
+	.af_name	= "mpls",
+	.af_af		= AF_MPLS,
+	.af_status	= mpls_status,
+	.af_getaddr	= mpls_getaddr,
+	.af_difaddr	= SIOCDIFADDR,
+	.af_aifaddr	= SIOCAIFADDR,
+	.af_ridreq	= &mpls_addreq,
+	.af_addreq	= &mpls_addreq,
+};
+
+/* 
+ * Attach this module to ifconfig(8). 
+ */
+static __constructor void
+mpls_ctor(void)
+{
+#define	N(a)	(sizeof(a) / sizeof(a[0]))
+	int i;
+ 
+	for (i = 0; i < N(mpe_cmds);  i++) 
+		cmd_register(&mpe_cmds[i]);
+	af_register(&af_mpls);
+#undef N	
+}
+
---
+++ sbin/ifconfig/ifmpe.c
@@ -0,0 +1,107 @@
+/*-
+ * Copyright (c) 2015 Henning Matyschok
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <sys/param.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/sockio.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <net/if_types.h>
+#include <net/route.h>
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <errno.h>
+
+#include "ifconfig.h"
+
+#include <netmpls/mpls.h>
+
+static void
+mpe_status(int s)
+{
+	struct if_laddrreq iflr;
+	struct sockaddr_mpls *smpls;
+	struct sockaddr_dl *sdl;
+	
+	(void)memset(&iflr, 0, sizeof(iflr));
+	(void)memcpy(iflr.iflr_name, name, IFNAMSIZ);	
+	
+	smpls = (struct sockaddr_mpls *)&iflr.addr;
+	smpls->smpls_len = sizeof(*smpls);
+	smpls->smpls_family = AF_MPLS;
+	
+	sdl = (struct sockaddr_dl *)&iflr.dstaddr;
+	sdl->sdl_len = sizeof(*sdl);
+	sdl->sdl_family = AF_LINK;	
+	
+	if (ioctl(s, SIOCGLIFPHYADDR, &iflr) < 0)  
+		return;
+/*
+ * Following code-section is reused from implementation 
+ * of in af_link.c defined link_status procedure, part of
+ * ifconfig(8) implementation. 
+ */		
+	(void)printf("\tlink ");
+		
+	if (sdl != NULL && sdl->sdl_alen > 0) {
+		if ((sdl->sdl_type == IFT_ETHER 
+			|| sdl->sdl_type == IFT_L2VLAN 
+			|| sdl->sdl_type == IFT_BRIDGE) 
+			&& sdl->sdl_alen == ETHER_ADDR_LEN) {
+			
+			(void)printf("%s ",
+				ether_ntoa((struct ether_addr *)LLADDR(sdl)));
+		} else {
+			int n = sdl->sdl_nlen > 0 ? sdl->sdl_nlen + 1 : 0;
+			
+			(void)printf("%s ", link_ntoa(sdl) + n);
+		}
+	}	
+}
+
+static struct afswtch af_mpe = {
+	.af_name	= "af_mpe",
+	.af_af		= AF_MPLS,
+	.af_other_status = mpe_status,
+};
+
+static __constructor void
+mpe_ctor(void)
+{
+	
+	af_register(&af_mpe);
+}
